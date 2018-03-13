c3de659ca2df2eeaaac5d6d56fc2054536807095
Niko <niko.delarich@gmail.com>
2018-03-13 19:15:51 +0100

    Final Version for now
    Worked great!

--- node_helper.js
+++ node_helper.js
@@ -6,28 +6,47 @@
  * MIT Licensed.
  */
 
+var fs  = require('fs');
+ 
 const NodeHelper = require('node_helper');
 module.exports = NodeHelper.create({
 	start: function () {
-		this.loaded = false;
+		
 	},
 	// Subclass socketNotificationReceived received.
 	socketNotificationReceived: function(notification, payload) {
 		const self = this;
 		this.config = payload;
-		if (notification == 'VIDEO_CHANGED') {		
+		if (notification == 'PLAY_VIDEO') {		
+			var omx = require('omxdirector');
+			this.config = payload;	
+			var status = omx.getStatus();
+			if(status.playing){
+				//omx.stop();
+				//Do nothing
+			} else {
+				console.log(self.config.currentUser + " is logged in");
+				
+				var folder = self.config.videoFolder;
+				if(self.config.userFolders.indexOf(self.config.currentUser) != -1) {
+					folder += "/" + self.config.currentUser;
+				} else {
+					folder += "/stranger";
+				}
+				console.log("Playing files from " + folder);
+				
+				var files = fs.readdirSync(folder);
+				var randomItem = files[Math.floor(Math.random() * files.length)];
+				console.log("Playing " + randomItem);
+				
+				omx.play(folder + "/" + randomItem);
+			}
+		} else if (notification == 'STOP_VIDEO') {		
 			var omx = require('omxdirector');
 			this.config = payload;	
 			var status = omx.getStatus();
 			if(status.playing){
 				omx.stop();
-			}  
-			else if(this.loaded){
-				//TODO
-				omx.play("modules/MMM-RandomVideo/videos/vid1.mp4");
-			}
-			else{
-				setTimeout(function(){this.socketNotificationReceived(notification, payload)},2000);
 			}
 		}
 	}
